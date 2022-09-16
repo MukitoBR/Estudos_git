7f11758045eb8381a54d68d0289bfc5d15d54fbc
Fabiano Santos Ramos <113201492+MukitoBR@users.noreply.github.com>
2022-09-16 18:20:27 -0300

    Estudo para Git

---
+++ HelloWolrd.py
@@ -0,0 +1,17 @@
+
+from tkinter import Y
+
+
+#x = "Meu primeiro Programa"
+#y = "Salvo pelo mundo"
+
+#print(x + ' ' + y)
+
+primeiro_numero = int(input('Digite um numero: '))
+segundo_numero = int(input("Digite o Segundo numero: "))
+
+print("Voce digitou os numeros: ")
+print( str(primeiro_numero) + " E O numero " + str(segundo_numero))
+
+
+
